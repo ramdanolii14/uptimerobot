a1ec39af6c65f5e19685c2c26d0473ca4c804ee6
ramdanolii14 <65667464+ramdanolii14@users.noreply.github.com>
2020-08-23 16:54:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://bot-nakanomiku.glitch.me"]
+const urls = ["https://bot-nakanomiku-2.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
